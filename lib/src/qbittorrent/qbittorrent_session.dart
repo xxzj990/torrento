5626b6a9844cde47f3ab9ff69a10f45731af6401
liu <lllei@szhq.com>
2021-09-03 12:06:34 +0800

    proxy post multi

--- lib/src/qbittorrent/qbittorrent_session.dart
+++ lib/src/qbittorrent/qbittorrent_session.dart
@@ -10,17 +10,10 @@ class Session implements IQbitTorrentSession {
 
   @override
   Future<http.Response> get(String url, {Map<String, String>? headers}) async {
-    /// headers receives as per the headers arg is sent to the API>
+    var proxyBody = {'url': Uri.encodeFull(url)};
 
-    var proxyBody = {
-      'url': url,
-    };
-
-    http.Response response = await http.post(Uri.parse('http://localhost:8080/request'), body: proxyBody, headers: sessionHeaders);
-
-    //http.Response response = await http.get(Uri.parse(url), headers: sessionHeaders);
+    http.Response response = await http.post(Uri.parse('http://localhost:8080/request'), body: json.encode(proxyBody), headers: sessionHeaders);
     _updateCookie(response);
-    //log('status : ${response.statusCode} , response body : ' + response.body);
     return response;
   }
 
@@ -33,28 +26,20 @@ class Session implements IQbitTorrentSession {
       }
     });
 
-    var proxyBody = {
-      'url': url,
-      'body': json.encode(copyBody),
-    };
-    http.Response response = await http.post(Uri.parse('http://localhost:8080/request'), body: proxyBody, headers: sessionHeaders, encoding: encoding);
-    //http.Response response = await http.post(Uri.parse(url), body: copyBody, headers: sessionHeaders, encoding: encoding);
-    //log('status : ${response.statusCode} , response body : ' + response.body);
+    var proxyBody = {'url': Uri.encodeFull(url), 'body': copyBody};
+
+    http.Response response = await http.post(Uri.parse('http://localhost:8080/request'), body: json.encode(proxyBody), headers: sessionHeaders, encoding: encoding);
     _updateCookie(response);
     return response;
   }
 
-  void _updateCookie(http.Response response) {
-    String? rawCookie = response.headers['set-cookie'];
-    if (rawCookie != null) {
-      int index = rawCookie.indexOf(';');
-      sessionHeaders['cookie'] = (index == -1) ? rawCookie : rawCookie.substring(0, index);
-    }
-  }
-
   @override
   Future<http.StreamedResponse> postMulti(String url, Map<String, dynamic> fields, List<http.MultipartFile> files) async {
-    var request = http.MultipartRequest('POST', Uri.parse(url));
+
+    var request = http.MultipartRequest('POST', Uri.parse('http://localhost:8080/requestMulti'));
+    request.fields['url'] = Uri.encodeFull(url);
+
+    //var request = http.MultipartRequest('POST', Uri.parse(url));
     if (fields.isNotEmpty) {
       fields.forEach((key, value) {
         request.fields[key] = '$value';
@@ -69,6 +54,15 @@ class Session implements IQbitTorrentSession {
     var response = await request.send();
     _updateCookie2(response);
     return response;
+  }
+
+
+  void _updateCookie(http.Response response) {
+    String? rawCookie = response.headers['set-cookie'];
+    if (rawCookie != null) {
+      int index = rawCookie.indexOf(';');
+      sessionHeaders['cookie'] = (index == -1) ? rawCookie : rawCookie.substring(0, index);
+    }
   }
 
   void _updateCookie2(http.StreamedResponse response) {
